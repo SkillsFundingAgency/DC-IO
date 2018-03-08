983d9fa47b881f8e32aa46a03bea630f0774b3ab
Mike Childs <mike.childs@education.gov.uk>
2018-03-08 12:37:12 +0000

    Interfaces Project and IKeyValuePairStoreService

---
+++ src/ESFA.DC.IO.Interfaces/IKeyValuePairStoreService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ESFA.DC.IO.Interfaces
+{
+    public interface IKeyValuePairStoreService
+    {
+        Task<bool> SaveAsync(string key, string value);
+
+        Task<string> GetAsync(string key);
+
+        Task<bool> RemoveAsync(string key);
+    }
+}
