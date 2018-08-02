e978e9ca9081baa4f529e1e49dca2125c7e1e79e
John Youren <john.youren@gmail.com>
2018-08-02 10:19:27 +0100

    Added cancellation token support and stream support

--- src/ESFA.DC.IO.Interfaces/IKeyValuePersistenceService.cs
+++ src/ESFA.DC.IO.Interfaces/IKeyValuePersistenceService.cs
@@ -1,15 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace ESFA.DC.IO.Interfaces
 {
     public interface IKeyValuePersistenceService
     {
-        Task SaveAsync(string key, string value);
+        Task SaveAsync(string key, string value, CancellationToken cancellationToken = default(CancellationToken));
 
-        Task<string> GetAsync(string key);
+        Task<string> GetAsync(string key, CancellationToken cancellationToken = default(CancellationToken));
 
-        Task RemoveAsync(string key);
+        Task RemoveAsync(string key, CancellationToken cancellationToken = default(CancellationToken));
 
-        Task<bool> ContainsAsync(string key);
+        Task<bool> ContainsAsync(string key, CancellationToken cancellationToken = default(CancellationToken));
     }
 }
---
+++ src/ESFA.DC.IO.Interfaces/IStreamableKeyValuePersistenceService.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.IO.Interfaces
+{
+    public interface IStreamableKeyValuePersistenceService : IKeyValuePersistenceService
+    {
+        Task SaveAsync(string key, Stream value, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task GetAsync(string key, Stream value, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
